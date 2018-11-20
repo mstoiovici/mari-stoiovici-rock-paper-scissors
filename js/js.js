e8438df857f8e408894a2110038df617de3acf69
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2018-11-20 11:56:35 +0000

    deleted unused code from comments

--- js/js.js
+++ js/js.js
@@ -12,9 +12,8 @@ $('button').click(function(event) {
   $("#play_you").text(play_you);
   var play_machine = computer_move();
   compare(play_you, play_machine);
-
 });
-
+/*......../listen to the clicks.................................*/
 
 function computer_move() {
   var play_machine = Math.floor(Math.random() * (3 - 0)) + 0;
@@ -23,9 +22,8 @@ function computer_move() {
   /*console.log("move: " + play_machine);*/
   $("#play_machine").text(play_machine);
   return play_machine;
-
-
 }
+/*......../computer moves.........................................*/
 
 function compare(you, machine) {
   verdict = "lose";
@@ -35,45 +33,28 @@ function compare(you, machine) {
   if (((you == "rock") && (machine == "scissors")) || ((you == "paper") && (machine == "rock")) || ((you == "scissors") && (machine == "paper"))) {
     verdict = "win";
   }
-
   /*console.log("move: " + verdict);*/
-
-
-
-
-
+  /*......../compare moves...........................................*/
   switch (verdict) {
     case 'win':
       won++;
-      console.log("move: " + won);
       $("#game_won").text(won);
       break;
 
     case 'lose':
       lost++;
-      console.log("move: " + lost);
       $("#game_lost").text(lost);
       break;
 
     case 'draw':
       draw++;
-      console.log("move: " + draw);
       $("#game_draw").text(draw);
       break;
   }
-  /*if(verdict=="win"){
-    won++;
-    $("#game_won").text(won);
-  }
-  if(verdict=="lose"){
-    lost++;
-    $("#game_lost").text(lost);
-  }
-  if(verdict=="draw"){
-    draw++;
-    $("#game_draw").text(draw);
-  }*/
+/*......../updates the wons, losts and draws ...........................................*/
 $("#verdict").text(verdict);
   played++;
   $("#game_played").text(played);
+  /*......../ returns the verdict andupdates the nr of played games ...........................................*/
 }
+  /*......../compare moves and updates variables...........................................*/
